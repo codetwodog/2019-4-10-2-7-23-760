682be544335c88b7e3b08b9b6966bb22dc172056
571001265@qq.com <571001265@qq.com>
2019-05-24 02:08:23 +0800

    TDD_v0.1

--- main/main.js
+++ main/main.js
@@ -1,4 +1,5 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    if(inputs.distance<=2) return Math.round(6+0.25*inputs.parkTime);
+    else if(inputs.distance>=2&&inputs.distance<=8) return Math.round(6+0.8*(inputs.distance-2)+0.25*inputs.parkTime);
+    else {return Math.round(6+0.8*(8-2)+(inputs.distance-8)*(0.8*(1+0.5))+0.25*inputs.parkTime);}
 };
